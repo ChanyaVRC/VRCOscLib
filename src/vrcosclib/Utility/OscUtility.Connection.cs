e33acf8fe410da11c63037e98fbd61ac3977128a
ChanyaKushima <63221603+ChanyaVRC@users.noreply.github.com>
2022-02-24 21:33:53 +0900

    Upgrade: Allows the user to configure send/receive port. #2

--- src/vrcosclib/Utility/OscUtility.Connection.cs
+++ src/vrcosclib/Utility/OscUtility.Connection.cs
@@ -6,4 +6,43 @@ public static partial class OscUtility
 {
     private static OscServer? _server;
     private static OscClient? _client;
+
+    public static OscServer Server => _server ??= OscServer.GetOrCreate(_sendPort);
+    public static OscClient Client => _client ??= new OscClient("127.0.0.1", _receivePort);
+
+    private static int _sendPort = 9001;
+    public static int SendPort
+    {
+        get => _sendPort;
+        set
+        {
+            if (_server != null)
+            {
+                throw new InvalidOperationException("Server is working; cannot change port after to start.");
+            }
+            if (value > 65535 || value < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(value));
+            }
+            _sendPort = value;
+        }
+    }
+
+    private static int _receivePort = 9000;
+    public static int ReceivePort
+    {
+        get => _receivePort;
+        set
+        {
+            if (_client != null)
+            {
+                throw new InvalidOperationException("Client is working; cannot change port after to start.");
+            }
+            if (value > 65535 || value < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(value));
+            }
+            _receivePort = value;
+        }
+    }
 }
