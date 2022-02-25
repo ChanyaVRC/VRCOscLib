e2124200a8dd2dba3418f7165b49723ca4bbfc21
ChanyaKushima <63221603+ChanyaVRC@users.noreply.github.com>
2022-02-26 02:36:38 +0900

    Update: Update sample, LogAvatarParameters.

--- Sample/LogAvatarParametars/Program.cs
+++ Sample/LogAvatarParametars/Program.cs
@@ -2,15 +2,25 @@
 
 OscAvatarConfig? avatarConfig = null;
 
-Console.WriteLine("Reading now... Try to \"Reset Avatar.\"");
+Console.WriteLine($"[NOTIFICATION] Reading now... Try to \"Reset Avatar.\"");
 avatarConfig = await OscAvatarConfig.WaitAndCreateCurrentOscAvatarConfigAsync();
-Console.WriteLine($"Read avatar config. Name: {avatarConfig.Name}");
+Console.WriteLine($"[NOTIFICATION] Read avatar config. Name: {avatarConfig.Name}");
 
-avatarConfig.Parameters.ParameterChanged += (parameter, e) =>
- {
-     DateTime now = DateTime.Now;
-     Console.WriteLine($"[{now.ToShortDateString()} {now.ToShortTimeString()}] " +
-         $"{parameter.Name}: {e.OldValue} => {e.NewValue}");
- };
+OscAvatarParameterChangedEventHandler? handler = (parameter, e) =>
+{
+    DateTime now = DateTime.Now;
+    Console.WriteLine($"[{now.ToShortDateString()} {now.ToShortTimeString()}] " +
+        $"{parameter.Name}: {e.OldValue} => {e.NewValue}");
+};
+OscAvatarUtility.AvatarChanged += (sender, e) =>
+{
+    avatarConfig.Parameters.ParameterChanged -= handler;
+
+    avatarConfig = OscAvatarConfig.CreateCurrentOscAvatarConfig()!;
+    Console.WriteLine($"[NOTIFICATION] Changed avatar. Name: {avatarConfig.Name}");
+
+    avatarConfig.Parameters.ParameterChanged += handler;
+};
+avatarConfig.Parameters.ParameterChanged += handler;
 
 await Task.Delay(-1);
