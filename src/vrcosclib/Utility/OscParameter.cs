3835f0b6ac7e88c272c8e1cc70b99297421abf8e
ChanyaKushima <63221603+ChanyaVRC@users.noreply.github.com>
2022-03-02 18:33:10 +0900

    Update: Enabled to get the event of parameter change.

--- src/vrcosclib/Utility/OscParameter.cs
+++ src/vrcosclib/Utility/OscParameter.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using ParamChangedHandler = BuildSoft.VRChat.Osc.OscParameterChangedEventHandler<BuildSoft.VRChat.Osc.IReadOnlyOscParameterCollection>;
 
 namespace BuildSoft.VRChat.Osc;
 public static partial class OscParameter
 {
     private static OscParameterCollection? _parameters;
     internal static OscParameterCollection Parameters => _parameters ??= new();
+
+    public static event ParamChangedHandler ValueChanged
+    {
+        add => Parameters.ValueChanged += value;
+        remove => Parameters.ValueChanged -= value;
+    }
 
     static OscParameter()
     {
