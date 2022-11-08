064a0e1ab94b84682a0d7fba610b59a93144fe14
ChanyaKushima <63221603+ChanyaVRC@users.noreply.github.com>
2022-11-09 00:26:57 +0900

    Add a sample for chatbox.

---
+++ Sample/ChatboxSender/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using BuildSoft.VRChat.Osc.Chatbox;
+
+while (true)
+{
+    Console.Write("Text: ");
+    string? text = Console.ReadLine();
+    if (text == null)
+    {
+        Console.WriteLine("Cannot Send Text.");
+        continue;
+    }
+    if (text != "")
+    {
+        OscChatbox.SetIsTyping(true);
+        await Task.Delay(1000);
+        OscChatbox.SendMessage(text, true);
+        OscChatbox.SetIsTyping(false);
+    }
+}
