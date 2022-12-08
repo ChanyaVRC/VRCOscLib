f0a081c94aab93b9b437a31ca396405948ad3935
ChanyaKushima <63221603+ChanyaVRC@users.noreply.github.com>
2022-12-09 00:06:39 +0900

    Implemented OSC Trackers. #32
    See https://docs.vrchat.com/docs/osc-trackers

---
+++ src/VRCOscLib/VRCOscLib/Tracking/OscTracking.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Immutable;
+
+namespace BuildSoft.VRChat.Osc.Tracking;
+
+public class OscTracking
+{
+    private static OscTracker? _headTracker;
+    private static readonly Lazy<ImmutableArray<OscTracker>> _trackers = new(CreateTrackers);
+
+    public static OscTracker HeadTracker => _headTracker ??= new OscTracker("head");
+    public static ImmutableArray<OscTracker> Trackers => _trackers.Value;
+
+    private static ImmutableArray<OscTracker> CreateTrackers()
+    {
+        var trackers = ImmutableArray.CreateBuilder<OscTracker>(OscTracker.SupportedTrackerCount);
+        for (int i = 0; i < OscTracker.SupportedTrackerCount; i++)
+        {
+            trackers.Add(new OscTracker(i));
+        }
+        return trackers.ToImmutable();
+    }
+
+    private OscTracking()
+    {
+    }
+}
