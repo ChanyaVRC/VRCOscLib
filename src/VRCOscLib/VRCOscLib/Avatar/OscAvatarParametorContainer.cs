667c37f2f3549d5aa42f2c8136a80b1dc076a4dc
ChanyaKushima <63221603+ChanyaVRC@users.noreply.github.com>
2023-05-17 22:32:08 +0900

    Added new VRCPhysBone's parameters.

--- src/VRCOscLib/VRCOscLib/Avatar/OscAvatarParametorContainer.cs
+++ src/VRCOscLib/VRCOscLib/Avatar/OscAvatarParametorContainer.cs
@@ -146,8 +146,10 @@ public class OscAvatarParametorContainer : IReadOnlyDictionary<string, object?>
         (string Suffix, OscType Type)[] paramInfos =
         {
             ("_" + nameof(OscPhysBone.IsGrabbed),   OscType.Bool),
+            ("_" + nameof(OscPhysBone.IsPosed),     OscType.Bool),
             ("_" + nameof(OscPhysBone.Angle),       OscType.Float),
             ("_" + nameof(OscPhysBone.Stretch),     OscType.Float),
+            ("_" + nameof(OscPhysBone.Squish),      OscType.Float),
         };
 
         Dictionary<string, int> dictionay = new();
