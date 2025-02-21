b52d12d270b898fab394fe43502ab20a20216a9d
ChanyaVRC <63221603+ChanyaVRC@users.noreply.github.com>
2025-02-21 19:39:39 +0900

    Implement an avatar change. (#46)
    Implementation for #43

--- src/VRCOscLib/VRCOscLib/Avatar/OscAvatar.cs
+++ src/VRCOscLib/VRCOscLib/Avatar/OscAvatar.cs
@@ -1,4 +1,6 @@
-﻿namespace BuildSoft.VRChat.Osc.Avatar;
+﻿using System.Security.Cryptography;
+
+namespace BuildSoft.VRChat.Osc.Avatar;
 
 /// <summary>
 /// Represents a VRChat avatar, as identified by its unique ID.
@@ -18,4 +20,39 @@ public struct OscAvatar
     /// or null if the avatar does not have a valid ID.
     /// </returns>
     public readonly OscAvatarConfig? ToConfig() => Id == null ? null : OscAvatarConfig.Create(Id);
+
+    /// <summary>
+    /// Changes an avatar in VRChat with <see cref="Id"/>.
+    /// </summary>
+    /// <exception cref="InvalidOperationException">Thrown if <see cref="Id"/> is <see langword="null"/>.</exception>
+    public void Change()
+    {
+        if (Id == null)
+        {
+            throw new InvalidOperationException("Id not set.");
+        }
+        OscAvatarUtility.ChangeAvatar(Id);
+    }
+
+    /// <inheritdoc/>
+    public override bool Equals(object? obj) => obj is OscAvatar avatar && Id == avatar.Id;
+
+    /// <inheritdoc/>
+    public override int GetHashCode() => 2108858624 + EqualityComparer<string?>.Default.GetHashCode(Id);
+
+    /// <summary>
+    /// Determines whether two specified <see cref="OscAvatar"/> have the same value.
+    /// </summary>
+    /// <param name="left">The first <see cref="OscAvatar"/> to compare.</param>
+    /// <param name="right">The second <see cref="OscAvatar"/> to compare.</param>
+    /// <returns></returns>
+    public static bool operator ==(OscAvatar left, OscAvatar right) => left.Id == right.Id;
+
+    /// <summary>
+    /// Determines whether two specified <see cref="OscAvatar"/> have different values.
+    /// </summary>
+    /// <param name="left">The first <see cref="OscAvatar"/> to compare.</param>
+    /// <param name="right">The second <see cref="OscAvatar"/> to compare.</param>
+    /// <returns></returns>
+    public static bool operator !=(OscAvatar left, OscAvatar right) => left.Id != right.Id;
 }
